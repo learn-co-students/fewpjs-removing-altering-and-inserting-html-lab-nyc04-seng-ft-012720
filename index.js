51d46dae0d27349e100607b5c054ed7b9a9b02eb
dagpan <panagiotisntagkinis@gmail.com>
2020-03-09 17:28:59 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+let element = document.getElementById('main')
+
+element.remove()
+
+element = document.querySelector('body')
+
+element.innerHTML = "<h1 id='victory'>YOUR-NAME is the champion</h1>";
+
+let newHeader = document.getElementById('victory')
