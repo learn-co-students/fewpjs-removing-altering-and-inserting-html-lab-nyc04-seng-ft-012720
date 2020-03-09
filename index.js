9109be767db4927aa91132833a5939ae9bd0618c
mav-code <mavisansmembres@gmail.com>
2020-03-09 17:53:35 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+let main = document.getElementById("main")
+
+main.remove()
+
+let newHeader = document.createElement('h1')
+
+document.body.appendChild(newHeader)
+
+newHeader.id = 'victory'
+
+newHeader.innerHTML = 'Mavi is the champion'
